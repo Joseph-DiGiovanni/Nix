0edb4a93d53ac06c3052fde86e32181315000657
Joseph DiGiovanni <jdigiovanni78@gmail.com>
2024-12-21 10:09:28 -0500

    Spacenavd autostart fix

--- nixos/software/spacenavd.nix
+++ nixos/software/spacenavd.nix
@@ -3,4 +3,6 @@
 {
   environment.systemPackages = with pkgs; [ spnavcfg ];
   hardware.spacenavd.enable = true;
+  # TODO: Remove once https://github.com/NixOS/nixpkgs/pull/366768 is merged into stable
+  systemd.services.spacenavd.wantedBy = [ "graphical.target" ];
 }
