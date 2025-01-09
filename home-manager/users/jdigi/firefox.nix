c22a4fe5a1be6efe54e5b8f8722ef07012e5acd6
Joseph DiGiovanni <jdigiovanni78@gmail.com>
2025-01-08 22:16:42 -0500

    Add Ultrawidify firefox extension

--- home-manager/users/jdigi/firefox.nix
+++ home-manager/users/jdigi/firefox.nix
@@ -4,21 +4,29 @@ in
 {
   programs.firefox.policies.ExtensionSettings = {
     "{446900e4-71c2-419f-a6a7-df9c091e268b}" = {
+      # Bitwarden Password Manager
       default_area = "navbar";
       install_url = latestSourceURL "bitwarden-password-manager";
-      installation_mode = "force_installed";
+      installation_mode = "normal_installed";
     };
 
-    "simple-tab-groups@drive4ik" = {
-      default_area = "navbar";
-      install_url = latestSourceURL "simple-tab-groups";
-      installation_mode = "force_installed";
+    "{cf02b1a7-a01a-4e37-a609-516a283f1ed3}" = {
+      # Ultrawidify
+      default_area = "menupanel";
+      install_url = latestSourceURL "ultrawidify";
+      installation_mode = "normal_installed";
     };
 
     "addon@darkreader.org" = {
       default_area = "menupanel";
       install_url = latestSourceURL "darkreader";
-      installation_mode = "force_installed";
+      installation_mode = "normal_installed";
+    };
+
+    "simple-tab-groups@drive4ik" = {
+      default_area = "navbar";
+      install_url = latestSourceURL "simple-tab-groups";
+      installation_mode = "normal_installed";
     };
   };
 }
